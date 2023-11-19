5a5e037910a53e6e7879ded0e0e05d72d4659823
Ashim Kumar Shaha <ashimkumar69@gmail.com>
2023-11-20 01:20:58 +0600

    group module create

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GroupsModule],
 })
 export class AppModule {}
